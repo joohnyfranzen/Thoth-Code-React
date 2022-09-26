4b0d1081fbab4e6b49d1b06ff386220616ac6476
Jonathan <joohny_2014@hotmail.com>
2022-09-26 13:29:17 -0300

    Axios Bearer Token

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { store } from "..";
 
 export default function Axios(){
+    const {value} = store.getState().Auth;
+
     const http = axios.create({
         baseURL:"http://localhost:8000/api",
         headers:{
-            "Content-type" : "application/json"
+            "Content-type" : "application/json",
+            "Authorization" : `Bearer ${value}`
         }
     });
     return {
