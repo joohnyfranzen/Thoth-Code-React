323e33059282f34ef327511810b66b74b1b7703f
Jonathan <joohny_2014@hotmail.com>
2022-09-26 13:29:45 -0300

    Newpost route

--- src/routes/Logged.routes.js
+++ src/routes/Logged.routes.js
@@ -3,6 +3,7 @@ import Logout from "../components/Logout";
 import Navbar from "../components/Navbar_2";
 import Dashboard from "../Screens/Dashboard";
 import Home from "../Screens/Home";
+import NewPost from "../Screens/Posts/NewPost";
 
 
 export default function Logged() {
@@ -13,6 +14,7 @@ export default function Logged() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/logout" element={<Logout/>}/>
+                <Route path="/newpost" element={<NewPost/>}/>
             </Routes>
         </Router>
     )
