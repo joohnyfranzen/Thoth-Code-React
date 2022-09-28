aabf28eeb003bd7c720b0b980c9192c98ef9c7c2
Jonathan <joohny_2014@hotmail.com>
2022-09-28 15:38:35 -0300

    Logged Routes edited

--- src/routes/Logged.routes.js
+++ src/routes/Logged.routes.js
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Logout from "../components/Logout";
 import Navbar from "../components/Navbar_2";
 import Dashboard from "../Screens/Dashboard";
-import Home from "../Screens/Home";
-import NewPost from "../Screens/Posts/NewPost";
+import Home from "../Screens/Home/Home";
+import EditPost from "../Screens/Posts/EditPost/EditPost";
+import MyPosts from "../Screens/Posts/MyPosts/MyPosts";
+import NewPost from "../Screens/Posts/NewPost/NewPost";
+
 
 
 export default function Logged() {
@@ -15,6 +18,8 @@ export default function Logged() {
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/logout" element={<Logout/>}/>
                 <Route path="/newpost" element={<NewPost/>}/>
+                <Route path="/myposts" element={<MyPosts/>}/>
+                <Route path="/myposts/:id" element={<EditPost/>}/>
             </Routes>
         </Router>
     )
