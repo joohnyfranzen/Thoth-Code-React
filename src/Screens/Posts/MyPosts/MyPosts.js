a4448c960de1f7b597fc133c010fbf85ffe6c3da
Jonathan <joohny_2014@hotmail.com>
2022-09-28 17:11:56 -0300

    Function show something if !post

--- src/Screens/Posts/MyPosts/MyPosts.js
+++ src/Screens/Posts/MyPosts/MyPosts.js
@@ -1,6 +1,6 @@
 import Axios from "../../../components/Axios";
 import { React, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { store } from "../../..";
 import './MyPosts.css'
 
@@ -9,6 +9,7 @@ export default function MyPosts() {
 
     const {http} = Axios();
 
+    const navigate = useNavigate();
 
     const [posts, setPosts] = useState([]);
 
@@ -32,29 +33,47 @@ export default function MyPosts() {
     return (
         <div className="main">
 
-        <div className="title">
-            <h1 className="title-posts">My Code Posts</h1>
-        </div>
-        <div className="posts">
-            {posts.map((post) => {
-            return (
-                <div className="post-container">
-                    <h2 className="post-title">{post.title}</h2>
-                    <p>{post.content}</p>
-                    <p>{post.user_id}</p>
-                    <h3 className="slug">{post.slug}</h3>
-                    <Link className="nav-button" to={`/myposts/${post.id}`}>Edit</Link>
-                    <Link className="nav-button" to={`/myposts/${post.id}`}>Delete</Link>
+            <div className="title">
+                <h1 className="title-posts">My Code Posts</h1>
+            </div>
+            { posts[0] ?
+            (
+                <div>
+                    <div className="posts">
+                        {posts.map((post) => {
+                        const del = () => {
+                            http.delete(`/post/${post.id}`).then(() => {
+                                window.location.reload(false)
+                            })          
+                        }
+                        return (
+                            <div className="post-container">
+                                <h2 className="post-title">{post.title}</h2>
+                                <p>{post.content}</p>
+                                <p>{post.user_id}</p>
+                                <h3 className="slug">{post.slug}</h3>
+                                <Link className="nav-button" to={`/myposts/${post.id}`}>Edit</Link>
+                                <button type="button" onClick={del} className="btn-danger del-button">Delete</button>
+                            </div> 
+                        );
+                    })}
+                    </div>
+                    <div className="pages">
+                        <button className="page-btn" onClick={() => {paginate(pagePaginate-1)}  }> {'<<'} </button>
+                        <button className="page-btn" onClick={() => {paginate(pagePaginate+1)}  }> {'>>'} </button>
+                    </div>
+                </div>
+            ) : 
+            (
+                <div>
+                    <h2>Oh, perharps you want to start posting something ?!?
+                    </h2>
+                    <Link className="nav-button" to={'/newpost'}>Click Here</Link> 
+                </div>
+            )
+            }  
 
-                </div> 
-            );
-            })}
-        </div>
-        <div className="pages">
-            <button className="page-btn" onClick={() => {paginate(pagePaginate-1)}  }> {'<<'} </button>
-            <button className="page-btn" onClick={() => {paginate(pagePaginate+1)}  }> {'>>'} </button>
-        </div>
       </div>    
    
-    )
+   )
 }
