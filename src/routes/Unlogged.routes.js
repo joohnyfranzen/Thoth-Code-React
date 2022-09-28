09ee9a843c8846995dd048ec7e29c6ec444172b9
Jonathan <joohny_2014@hotmail.com>
2022-09-28 15:38:58 -0300

    Unlogged Routes import edit

--- src/routes/Unlogged.routes.js
+++ src/routes/Unlogged.routes.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Home from "../Screens/Home";
+import Home from "../Screens/Home/Home";
 import Login from "../Screens/Login";
 import Register from "../Screens/Register";
 
